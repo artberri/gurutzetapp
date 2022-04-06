f7fa2cb393c2c89793123ecf6074a9c56146d4b4
Alberto Varela <alberto@berriart.com>
2022-04-06 23:36:49 +0200

    new app skeleton

---
+++ web/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "airbnb",
+    "airbnb/hooks",
+    "airbnb-typescript",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:unicorn/recommended",
+    "prettier",
+  ],
+  parserOptions: {
+    project: "./tsconfig.json",
+  },
+};
